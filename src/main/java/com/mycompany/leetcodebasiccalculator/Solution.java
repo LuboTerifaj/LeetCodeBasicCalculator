9c93683bd3f668ba94113fd0a20e7b82300bf922
Lubo Terifaj <lubo.terifaj@gmail.com>
2016-08-07 15:11:07 +0200

    Prepared project for solution.

---
+++ src/main/java/com/mycompany/leetcodebasiccalculator/Solution.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.leetcodebasiccalculator;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ *
+ * @author Lubo Terifaj
+ */
+public class Solution {
+    /**
+    * Implemented a basic calculator to evaluate a simple expression string.
+    * The expression string may contain open ( and closing parentheses ),
+    * the plus + or minus sign -, non-negative integers and empty spaces.
+    * Assume that the given expression is always valid.
+    * 
+    * @param s expression string which may contain '(', ')', ' ', '+', '-' and integers
+    * @return  result of expression
+    * 
+    */    
+    public int calculate(String s) {
+        throw new NotImplementedException();
+    }
+}
