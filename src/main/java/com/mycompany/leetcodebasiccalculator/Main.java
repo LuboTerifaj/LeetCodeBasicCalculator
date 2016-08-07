36803a74c1ad54517d6d9de0145bec9bb8306d43
Lubo Terifaj <lubo.terifaj@gmail.com>
2016-08-07 15:45:57 +0200

    Changed Main.main(String[] args).

--- src/main/java/com/mycompany/leetcodebasiccalculator/Main.java
+++ src/main/java/com/mycompany/leetcodebasiccalculator/Main.java
@@ -5,12 +5,31 @@
  */
 package com.mycompany.leetcodebasiccalculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Lubo Terifaj
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Enter expression: ");
         
+        String str = null;
+        
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        
+        try {
+            str = input.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Solution solution = new Solution();                
+        
+        System.out.println("Result of expression is: " + solution.calculate(str));
     }
 }
